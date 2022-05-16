34e311b742df55cb7a71caa0db7ffe938788115a
joaquintom <71027899+joaquintom@users.noreply.github.com>
2022-05-16 17:49:35 -0300

    2da Entrega

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,38 +1,154 @@
 
 
-class instrumento{
-    constructor(marca, origen, precio) {
-    this.marca = marca;
-    this.origen = origen;
-    this.precio = precio
+const productos = [
+
+    {id: 1, instrumento: "Bateria", marca: "ludwig", valor: 4000},
+    {id: 2, instrumento: "Bateria", marca: "pearl", valor: 3000},
+    {id: 3, instrumento: "Bateria", marca: "yamaha", valor: 2000},
+    {id: 4, instrumento: "Bateria", marca: "mapex", valor: 1000},
+    {id: 5, instrumento: "Guitarra", marca: "fender", valor: 3500},
+    {id: 6, instrumento: "Guitarra", marca: "gibson", valor: 3000},
+    {id: 7, instrumento: "Guitarra", marca: "ibanez", valor: 3500},
+    {id: 8, instrumento: "Guitarra", marca: "squire", valor: 2500},
+    {id: 9, instrumento: "Bajo", marca: "fender", valor: 2500},
+    {id: 10, instrumento: "Bajo", marca: "ibanez", valor: 1500},
+    {id: 11, instrumento: "Bajo", marca: "squier", valor: 1000},
+    {id: 12, instrumento: "Bajo", marca: "xs", valor: 500},
+    {id: 13, instrumento: "Teclado", marca: "roland", valor: 2000},
+    {id: 14, instrumento: "Teclado", marca: "yamaha", valor: 1800},
+    {id: 15, instrumento: "Teclado", marca: "m-audio", valor: 1500},
+    {id: 16, instrumento: "Teclado", marca: "casio", valor: 1000},
+
+]
+
+let carrito = []
+
+if(localStorage.getItem("carrito")){
+    carrito = JSON.parse(localStorage.getItem("carrito"))
+}else{
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+}
+
+
+//Dom
+let titulo1 = document.getElementById("titulo1")
+let divTitulo = document.getElementById("divTitulo")
+
+
+const buscador = document.querySelector("#inputBuscador");
+const botonB = document.querySelector("#botonBuscar");
+const items = document.querySelector("#divProductos")
+
+/*
+let botonChango = document.getElementById("botonCarrito")
+let botonVerCarrito = document.getElementById("botonMostrar")
+*/
+
+// modificaciones de  texto
+titulo1.innerText = "Instrumentos" 
+//nullis
+console.log(productos.material ?? "propiedad no encontrada") 
+// desestructuracion
+let {material} = productos
+console.log(material ?? "propiedad no encontrada")
+
+//buscador
+const filtrar = () =>{
+    items.innerHTML = ''; 
+    //console.log(inputBuscador.value)
+    const texto = buscador.value.toLowerCase();
+    for (let producto of productos){
+        let instrumento = producto.instrumento.toLowerCase();
+        if(instrumento.indexOf(texto) !== -1){
+            items.innerHTML +=  ` 
+            <div class="card" id="producto${producto.id}"style="width: 18rem;">
+                <div class="card-body">
+                <h3 class="card-title">${producto.instrumento}</h3>
+                    <h5 class="card-title">${producto.marca}</h5>
+                    <p class="card-text">$${producto.valor}</p>
+                    
+                    <button id="botonCarrito${producto.id}" class="btn btn-dark"> Agregar al carrito</button>
+                </div>
+            </div>
+            
+            `
+        }
+    }
+
+  if(items.innerHTML === ''){
+        items.innerHTML += `
+        <p> Producto no encontrado</p>
+        `
     }
 }
 
-const instrumento1 = new instrumento("rmv", "brasil", 500)
-const instrumento2 = new instrumento ("pearl", "Estados unidos" , 2000)
-const instrumento3 = new instrumento ("ludwig", "inglaterra", 4000)
+buscador.addEventListener("input", filtrar)
+botonB.addEventListener("click", filtrar)
+items.addEventListener("keyup", filtrar)
 
-let instrumentos = [instrumento1, instrumento2, instrumento3]
-
-console.log(instrumentos)
-
-for(let bateria of instrumentos)
-    for(let propiedad in bateria){
-    console.log(bateria[propiedad])
-}
+filtrar();
 
 
-/* 
-let instrumento = ["bateria", "guitarra", "piano"]
+//botonCarrito: agregar al carrito
+//botonMostrar: carrito
+// const items = divProductos
 
-let instrumentoAEliminar = prompt(" Elegir un instrumento a eliminar :\n bateria \n guitarra \n piano")
+//carrito
+    productos.forEach(producto => {
+    document.getElementById(`botonCarrito${producto.id}`).addEventListener("click", () =>{
+        carrito.push({producto})
+     localStorage.setItem("carrito", JSON.stringify(carrito))
+    })
+})
 
-let indice = instrumento.indexOf(instrumentoAEliminar)
 
-if(indice != -1){
-    instrumento.splice(indice,1)
-} else{
-    alert("no se encontro el instrumento a eliminar")
-}
-console.log(instrumento)
-*/
+//offcanva
+const chango = document.getElementById("offCanvaCarrito"); 
+ productos.forEach(producto => {
+            document
+                .getElementById(`botonCarrito${producto.id}`)
+                .addEventListener("click", () => {
+
+                    if (carrito.includes(producto)) {
+                        
+                    } else {
+                        chango.innerHTML += `
+                        <div class="card" id="producto${producto.id}"style="width: 18rem;">
+                        <div class="card-body">
+                        <h3 class="card-title">${producto.instrumento}</h3>
+                            <h5 class="card-title">${producto.marca}</h5>
+                            <p class="card-text">$${producto.valor}</p>
+                            <button id="eliminar${producto.id}" class="btn btn-dark"> Eliminar</button>
+                        </div>
+                    </div>
+                    `
+                    }
+                    
+             })})
+
+
+// mostrar los productos offcanva localstorage
+document.getElementById("botonMostrar").addEventListener("click", () =>{
+    let carritoStorage = JSON.parse(localStorage.getItem("carrito"))
+    //---console.log(arrayParseado)
+  
+    carritoStorage.forEach(producto => {
+        
+        chango.innerHTML += ` 
+        
+        <div class="card" id="producto${producto.id}"style="width: 18rem;">
+        <div class="card-body">
+        <h3 class="card-title">${producto.instrumento}</h3>
+            <h5 class="card-title">${producto.marca}</h5>
+            <p class="card-text">$${producto.valor}</p>
+                    
+                    <button id="eliminar${producto.id}" class="btn btn-dark"> Eliminar</button>
+                </div>
+            </div>
+        
+        `
+    
+    })
+})
+        
+
